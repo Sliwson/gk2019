3c1bd70ce1452e3bf53ab97ad1a0aa2838a11325
Sliwson <mateusz_sliw@interia.pl>
2019-10-06 11:50:38 +0200

    Rename image canvas to bitmap canvas as we'll be operating on bitmap

---
+++ gk2019/Common/BitmapCanvas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Common
+{
+    //represent const sized bitmap canvas
+    public class BitmapCanvas
+    {
+        private Bitmap bitmap;
+        public BitmapCanvas(Bitmap bitmap)
+        {
+            this.bitmap = bitmap;
+        }
+
+        public void Clear(Color color)
+        {
+            for (int i = 0; i < bitmap.Height; i++)
+                for (int j = 0; j < bitmap.Width; j++)
+                    bitmap.SetPixel(j, i, color);
+        }
+    }
+}
