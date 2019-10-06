2d0ce8decd2d7ada6237f225f290985bbdfbde16
Sliwson <mateusz_sliw@interia.pl>
2019-10-06 14:28:18 +0200

    Add extensions class to add functionalities to existing c# classes

---
+++ gk2019/Common/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace Common
+{
+    public static class PointExtensions
+    {
+        public static double DistanceTo(this Point p1, Point p2)
+        {
+            Point p3 = new Point(p1.X - p2.X, p1.Y - p2.Y);
+            return Math.Sqrt(Math.Pow(p3.X, 2) + Math.Pow(p3.Y, 2));
+        }
+    }
+}
