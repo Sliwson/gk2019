a24f6e4d9869ae771132cb1f0971d45d5f1ae61e
Sliwson <mateusz_sliw@interia.pl>
2019-10-06 14:28:32 +0200

    Add simple DrawPoint implementation

--- gk2019/Common/BitmapCanvas.cs
+++ gk2019/Common/BitmapCanvas.cs
@@ -9,17 +9,54 @@ namespace Common
     //represent const sized bitmap canvas
     public class BitmapCanvas
     {
+        public Color BackgroundColor { get; set; }
+
         private Bitmap bitmap;
         public BitmapCanvas(Bitmap bitmap)
         {
             this.bitmap = bitmap;
+
+            BackgroundColor = Color.White;
         }
 
-        public void Clear(Color color)
+        public bool IsPointOnBitmap(PointF position)
         {
+            return IsPointOnBitmap(Point.Round(position));
+        }
+
+        public bool IsPointOnBitmap(Point position)
+        {
+            return position.X >= 0 && position.X < bitmap.Width && position.Y >= 0 && position.Y < bitmap.Height;
+        }
+
+        public void Clear(Color? color = null)
+        {
+            Color clearColor = color ?? BackgroundColor;
+
             for (int i = 0; i < bitmap.Height; i++)
                 for (int j = 0; j < bitmap.Width; j++)
-                    bitmap.SetPixel(j, i, color);
+                    bitmap.SetPixel(j, i, clearColor);
+        }
+
+        public void DrawPoint(PointF position, float radius = 1f, Color? color = null)
+        {
+            Color drawColor = color ?? Color.Black;
+
+            Point startingPoint = Point.Round(position);
+            int r = (int)Math.Ceiling(radius);
+
+            for (int y = startingPoint.Y - r; y <= startingPoint.Y + r; y++)
+            {
+                for (int x = startingPoint.X - r; x <= startingPoint.X + r; x++)
+                {
+                    Point currentPoint = new Point(x, y);
+                    if (IsPointOnBitmap(currentPoint))
+                    {
+                        if (currentPoint.DistanceTo(startingPoint) <= radius)
+                            bitmap.SetPixel(currentPoint.X, currentPoint.Y, drawColor);
+                    }
+                }
+            }
         }
     }
 }
