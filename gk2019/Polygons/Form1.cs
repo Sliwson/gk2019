2940bbc46ff6892b3b59df62e2e9278837559425
Sliwson <mateusz_sliw@interia.pl>
2019-10-06 11:50:51 +0200

    Link project and perform simple test

--- gk2019/Polygons/Form1.cs
+++ gk2019/Polygons/Form1.cs
@@ -7,14 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Common;
 
 namespace Polygons
 {
     public partial class Form1 : Form
     {
+        private BitmapCanvas bitmapCanvas;
+
         public Form1()
         {
             InitializeComponent();
+
+            var bitmap = new Bitmap(canvas.Width, canvas.Height);
+            canvas.Image = bitmap;
+            bitmapCanvas = new BitmapCanvas(bitmap);
+            bitmapCanvas.Clear(Color.Yellow);
         }
     }
 }
