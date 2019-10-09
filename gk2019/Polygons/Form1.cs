dffa2e97ec4fbe7d40d030c002542f5bafd9c5bc
Mateusz Sliwakowski <mateusz@thingtrunk.com>
2019-10-09 20:05:06 +0200

    Prototype hierarchy display

--- gk2019/Polygons/Form1.cs
+++ gk2019/Polygons/Form1.cs
@@ -14,6 +14,7 @@ namespace Polygons
     public partial class Form1 : Form
     {
         private BitmapCanvas bitmapCanvas;
+        private List<Polygon> polygons = new List<Polygon>();
 
         public Form1()
         {
@@ -23,6 +24,39 @@ namespace Polygons
             canvas.Image = bitmap;
             bitmapCanvas = new BitmapCanvas(bitmap);
             bitmapCanvas.Clear(Color.Yellow);
+
+
+            for (int i = 0; i < 4; i++)
+            {
+                var p = Polygon.GetSampleSquare();
+                p.Move(new Point(i * 100, i * 100));
+                p.Draw(bitmapCanvas);
+                polygons.Add(p);
+            }
+
+            UpdateTreeView();
+        }
+
+        public void UpdateTreeView()
+        {
+            for (int i = 0; i < polygons.Count; i++)
+            {
+                var polygonNode = new TreeNode($"Polygon{i}");
+
+                var edgesNode = new TreeNode("Edges");
+                var verticesNode = new TreeNode("Vertices");
+
+                for (int j = 0; j < polygons[i].GetEdges().Count; j++)
+                    edgesNode.Nodes.Add($"Edge{j}");
+
+                for (int j = 0; j < polygons[i].GetVertices().Count; j++)
+                    verticesNode.Nodes.Add($"Vertex{j}");
+
+                polygonNode.Nodes.Add(edgesNode);
+                polygonNode.Nodes.Add(verticesNode);
+
+                hierarchy.Nodes.Add(polygonNode);
+            }
         }
     }
 }
