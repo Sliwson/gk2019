fcb4e4be293d0d6c01248a0d2574a128aab4f298
Mateusz Sliwakowski <mateusz@thingtrunk.com>
2019-10-09 19:19:14 +0200

    Implement basic polygon methods

--- gk2019/Common/Polygon.cs
+++ gk2019/Common/Polygon.cs
@@ -15,7 +15,12 @@ namespace Common
         void Draw(BitmapCanvas canvas);
     }
 
-    public class Polygon : IDrawable
+    public interface IMovable
+    {
+        void Move(Point offset);
+    }
+
+    public class Polygon : IDrawable, IMovable
     {
         public enum HitTestResult
         {
@@ -28,16 +33,94 @@ namespace Common
         private List<Edge> edges = new List<Edge>();
         private List<Vertex> vertices = new List<Vertex>();
 
-        public bool AddVertex(Vertex v)
-        {
+        private Vertex lastProcessedVertex = null;
+        private readonly double vertexRadius = 3;
 
+        public bool AddVertex(Point position)
+        {
+            var hitTest = HitTest(position);
+
+            //check vertex hittest
+            if (hitTest.Item1 == HitTestResult.Vertex)
+            {
+                Vertex targetVertex = hitTest.Item2 as Vertex;
+                if (IsVertexGoodForClosing(targetVertex))
+                {
+                    if (lastProcessedVertex == null)
+                        lastProcessedVertex = vertices[vertices.Count - 1];
+
+                    edges.Add(new Edge(lastProcessedVertex, targetVertex));
+                    lastProcessedVertex = targetVertex;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            //disallow adding vertices on edges
+            if (hitTest.Item1 == HitTestResult.Edge)
+                return false;
+
+            //add normal vertex
+            Vertex newVertex = new Vertex(position, vertexRadius);
+            if (lastProcessedVertex != null)
+                edges.Add(new Edge(lastProcessedVertex, newVertex));
+
+            lastProcessedVertex = newVertex;
+            return true;
         }
 
-        public HitTestResult HitTest(Point P)
+        public (HitTestResult, IMovable)  HitTest(Point position)
         {
+            foreach (var vertex in vertices)
+                if (vertex.HitTest(position))
+                    return (HitTestResult.Vertex, vertex);
 
+            foreach (var edge in edges)
+                if (edge.HitTest(position))
+                    return (HitTestResult.Edge, edge);
+
+            return (HitTestResult.Empty, null);
         }
 
-        private 
+        public void Draw(BitmapCanvas canvas)
+        {
+            foreach (var edge in edges)
+                edge.Draw(canvas);
+
+            foreach (var vertex in vertices)
+                vertex.Draw(canvas);
+        }
+        
+        public void Move(Point offset)
+        {
+            foreach (var edge in edges)
+                edge.Move(offset);
+        }
+
+        public static Polygon GetSampleSquare()
+        {
+            Polygon p = new Polygon();
+            p.AddVertex(new Point(10, 10));
+            p.AddVertex(new Point(10, 100));
+            p.AddVertex(new Point(100, 100));
+            p.AddVertex(new Point(100, 10));
+            p.AddVertex(new Point(10, 10));
+            return p;
+        }
+
+        private bool IsVertexGoodForClosing(Vertex v)
+        {
+            int neighbours = 0;
+
+            foreach (var edge in edges)
+                if (edge.Begin == v || edge.End == v)
+                    neighbours++;
+
+            //we can close rectangle if given vertex has one neighbour and we have at least 2 edges
+            return neighbours == 1 && edges.Count > 1;
+        }
     }
 }
