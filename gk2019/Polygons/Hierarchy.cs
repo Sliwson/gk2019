14d3176114375edee990934f95fb33c97ae03003
Mateusz Sliwakowski <mateusz@thingtrunk.com>
2019-10-09 23:29:29 +0200

    Prototype hierarchy context menus

--- gk2019/Polygons/Hierarchy.cs
+++ gk2019/Polygons/Hierarchy.cs
@@ -9,14 +9,24 @@ using Common;
 
 namespace Polygons
 {
+    public enum NodeType
+    {
+        Polygon,
+        EdgesList,
+        VerticesList,
+        Edge,
+        Vertex
+    }
 
     class GeometricNode : TreeNode
     {
         public PlaneStructure Structure { get; set; }
+        public NodeType Type { get; set; }
 
-        public GeometricNode (String name, PlaneStructure structure) : base (name)
+        public GeometricNode (String name, NodeType type, PlaneStructure structure) : base (name)
         {
             Structure = structure;
+            Type = type;
         }
     }
 
@@ -33,6 +43,49 @@ namespace Polygons
             this.polygonManager = polygonManager;
 
             treeView.AfterSelect += ItemSelected;
+            treeView.MouseClick += HierarchyClick;
+            treeView.MouseDown += HierarchyMouseDown;
+        }
+
+        private void HierarchyMouseDown(object sender, MouseEventArgs e)
+        {
+            if (treeView.HitTest(e.X, e.Y).Node != null)
+                return;
+
+            var contextMenu = new ContextMenuStrip();
+            contextMenu.Items.Add("Add polygon");
+            contextMenu.Show(treeView, e.X, e.Y);
+        }
+
+        private void HierarchyClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Right)
+                return;
+
+            var clickNode = treeView.GetNodeAt(e.X, e.Y);
+            treeView.SelectedNode = clickNode;
+            var knownNode = clickNode as GeometricNode;
+
+            var contextMenu = new ContextMenuStrip();
+            switch (knownNode.Type)
+            {
+                case NodeType.Edge:
+                    contextMenu.Items.Add("Remove");
+                    contextMenu.Items.Add("Split");
+                    break;
+                case NodeType.Vertex:
+                    contextMenu.Items.Add("Remove");
+                    break;
+                case NodeType.EdgesList:
+                    return;
+                case NodeType.VerticesList:
+                    return;
+                case NodeType.Polygon:
+                    contextMenu.Items.Add("Remove");
+                    break;
+            }
+
+            contextMenu.Show(treeView, e.X, e.Y);
         }
 
         public PlaneStructure GetStructureSelected()
@@ -42,21 +95,23 @@ namespace Polygons
 
         public void Update()
         {
+            treeView.Nodes.Clear();
             var polygons = polygonManager.GetPolygons();
+
             for (int i = 0; i < polygons.Count; i++)
             {
-                var polygonNode = new GeometricNode($"Polygon{i}", polygons[i]);
+                var polygonNode = new GeometricNode($"Polygon{i}", NodeType.Polygon, polygons[i]);
 
-                var edgesNode = new TreeNode("Edges");
-                var verticesNode = new TreeNode("Vertices");
+                var edgesNode = new GeometricNode("Edges", NodeType.EdgesList, null);
+                var verticesNode = new GeometricNode("Vertices", NodeType.VerticesList, null);
 
                 var edges = polygons[i].GetEdges();
                 for (int j = 0; j < edges.Count; j++)
-                    edgesNode.Nodes.Add(new GeometricNode($"Edge{j}", edges[j]));
+                    edgesNode.Nodes.Add(new GeometricNode($"Edge{j}", NodeType.Edge, edges[j]));
 
                 var vertices = polygons[i].GetVertices();
                 for (int j = 0; j < vertices.Count; j++)
-                    verticesNode.Nodes.Add(new GeometricNode($"Vertex{j}", vertices[j]));
+                    verticesNode.Nodes.Add(new GeometricNode($"Vertex{j}", NodeType.Vertex, vertices[j]));
 
                 polygonNode.Nodes.Add(edgesNode);
                 polygonNode.Nodes.Add(verticesNode);
@@ -67,14 +122,14 @@ namespace Polygons
 
         private void ItemSelected(object sender, TreeViewEventArgs e)
         {
-            if (!(e.Node is GeometricNode))
+            var node = e.Node as GeometricNode;
+            if (node.Type == NodeType.EdgesList || node.Type == NodeType.VerticesList)
             {
                 structureSelected = null;
                 return;
             }
 
             polygonManager.ClearDrawColor(Color.Black);
-            var node = e.Node as GeometricNode;
             structureSelected = node.Structure;
             node.Structure.DrawingColor = Color.Red;
 
