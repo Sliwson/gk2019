6024f09432e109cc766b77432e02f6dec313b7e7
Mateusz Śliwakowski <mateusz_sliw@interia.pl>
2019-11-02 11:15:10 +0100

    Bind slider variables

--- gk2019/Lightning/Form1.cs
+++ gk2019/Lightning/Form1.cs
@@ -20,30 +20,42 @@ namespace Lightning
         private void objectColorPictureBox_Click(object sender, EventArgs e)
         {
             if (objectColorDialog.ShowDialog() == DialogResult.OK)
-                objectColorPictureBox.BackColor = objectColorDialog.Color;
+            {
+                var color = objectColorDialog.Color;
+                objectColorPictureBox.BackColor = color;
+                Variables.ObjectColor.ObjectColor = color;
+            }
         }
 
         private void lightColorPicturebox_Click(object sender, EventArgs e)
         {
             if (lightColorDialog.ShowDialog() == DialogResult.OK)
+            {
+                var color = lightColorDialog.Color;
                 lightColorPictureBox.BackColor = lightColorDialog.Color;
+                Variables.Light.LightColor = color;
+            }
         }
 
        private void mSlider_ValueChanged(object sender, EventArgs e)
        {
             mTextbox.Text = mSlider.Value.ToString();
+            Variables.Coefficients.M = mSlider.Value;
        }
 
         private void kdSlider_ValueChanged(object sender, EventArgs e)
         {
-            var value = kdSlider.Value / 100.0;
+            var value = kdSlider.Value / 100.0f;
             kdTextbox.Text = value.ToString();
+            Variables.Coefficients.Kd = value;
         }
 
         private void ksSlider_ValueChanged(object sender, EventArgs e)
         {
-            var value = ksSlider.Value / 100.0;
+            var value = ksSlider.Value / 100.0f;
             ksTextbox.Text = value.ToString();
+            Variables.Coefficients.Ks = value;
         }
     }
 }
+
