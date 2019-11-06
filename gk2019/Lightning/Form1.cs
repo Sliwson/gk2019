68e3b674dd0ca4009be43adf2961361d08904a31
Mateusz Sliwakowski <mateusz@thingtrunk.com>
2019-11-06 20:19:46 +0100

    Add fast unsafe drawing function

--- gk2019/Lightning/Form1.cs
+++ gk2019/Lightning/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,17 +24,49 @@ namespace Lightning
         
         private void canvas_Paint(object sender, PaintEventArgs e)
         {
-            var colorArray = new Color[canvas.Height, canvas.Width];
+            Color[,] colorArray = new Color[canvas.Height, canvas.Width];
+            var triangles = grid.GetAllTriangles();
 
-            for (int y = 0; y < grid.GetSize().Height; y++)
-            {
-                for (int x = 0; x < 2 * grid.GetSize().Width; x++)
-                {
-                    drawer.FillPolygon(grid.GetTriangleVertices(x, y), e.Graphics);
-                }
-            }
+            Parallel.ForEach(triangles, triangle => {
+                drawer.FillPolygon(triangle, colorArray);
+            });
+            
+            var bitmap = new Bitmap(canvas.Width, canvas.Height, PixelFormat.Format24bppRgb);
+            ProcessUsingLockbitsAndUnsafeAndParallel(bitmap, colorArray);
+            var pixel = bitmap.GetPixel(10, 10);
 
+            e.Graphics.DrawImage(bitmap, 0, 0);
             grid.Paint(e.Graphics);
+        }
+        private void ProcessUsingLockbitsAndUnsafeAndParallel(Bitmap processedBitmap, Color[,] colorArray)
+        {
+            unsafe
+            {
+                BitmapData bitmapData = processedBitmap.LockBits(new Rectangle(0, 0, processedBitmap.Width, processedBitmap.Height), ImageLockMode.ReadWrite, processedBitmap.PixelFormat);
+
+                int bytesPerPixel = System.Drawing.Bitmap.GetPixelFormatSize(processedBitmap.PixelFormat) / 8;
+                int heightInPixels = bitmapData.Height;
+                int widthInBytes = bitmapData.Width * bytesPerPixel;
+                byte* PtrFirstPixel = (byte*)bitmapData.Scan0;
+
+                Parallel.For(0, heightInPixels, y =>
+                {
+                    byte* currentLine = PtrFirstPixel + (y * bitmapData.Stride);
+                    for (int x = 0; x < widthInBytes; x = x + bytesPerPixel)
+                    {
+                        int oldBlue = currentLine[x];
+                        int oldGreen = currentLine[x + 1];
+                        int oldRed = currentLine[x + 2];
+
+                        int colorX = x / bytesPerPixel;
+                        currentLine[x] = colorArray[y, colorX].R;
+                        currentLine[x + 1] = colorArray[y, colorX].G;
+                        currentLine[x + 2] = colorArray[y, colorX].B;
+                    }
+                });
+
+                processedBitmap.UnlockBits(bitmapData);
+            }
         }
     }
 }
