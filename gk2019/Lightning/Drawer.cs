65cc12ecd7d7fddbd0905e62d400cdfbeecf87aa
Mateusz Sliwakowski <mateusz@thingtrunk.com>
2019-11-06 20:57:18 +0100

    Move drawing function

--- gk2019/Lightning/Drawer.cs
+++ gk2019/Lightning/Drawer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -78,6 +79,37 @@ namespace Lightning
             else
             {
                 activeList.RemoveAll(x => x.YMax == current.Position.Y);
+            }
+        }
+
+        public static void WriteColormapToBitmap(Bitmap processedBitmap, Color[,] colorArray)
+        {
+            unsafe
+            {
+                BitmapData bitmapData = processedBitmap.LockBits(new Rectangle(0, 0, processedBitmap.Width, processedBitmap.Height), ImageLockMode.ReadWrite, processedBitmap.PixelFormat);
+
+                int bytesPerPixel = System.Drawing.Bitmap.GetPixelFormatSize(processedBitmap.PixelFormat) / 8;
+                int heightInPixels = bitmapData.Height;
+                int widthInBytes = bitmapData.Width * bytesPerPixel;
+                byte* PtrFirstPixel = (byte*)bitmapData.Scan0;
+
+                Parallel.For(0, heightInPixels, y =>
+                {
+                    byte* currentLine = PtrFirstPixel + (y * bitmapData.Stride);
+                    for (int x = 0; x < widthInBytes; x = x + bytesPerPixel)
+                    {
+                        int oldBlue = currentLine[x];
+                        int oldGreen = currentLine[x + 1];
+                        int oldRed = currentLine[x + 2];
+
+                        int colorX = x / bytesPerPixel;
+                        currentLine[x] = colorArray[y, colorX].R;
+                        currentLine[x + 1] = colorArray[y, colorX].G;
+                        currentLine[x + 2] = colorArray[y, colorX].B;
+                    }
+                });
+
+                processedBitmap.UnlockBits(bitmapData);
             }
         }
     }
--- gk2019/Lightning/Form1.cs
+++ gk2019/Lightning/Form1.cs
@@ -35,7 +35,7 @@ namespace Lightning
 
             using (var bitmap = new Bitmap(canvas.Width, canvas.Height, PixelFormat.Format24bppRgb))
             {
-                ProcessUsingLockbitsAndUnsafeAndParallel(bitmap, colorArray);
+                Drawer.WriteColormapToBitmap(bitmap, colorArray);
                 var pixel = bitmap.GetPixel(10, 10);
 
                 e.Graphics.DrawImage(bitmap, 0, 0);
@@ -46,36 +46,6 @@ namespace Lightning
             timer.Stop();
             var fps = 1000 / timer.ElapsedMilliseconds;
             this.Text = $"Lightning (Last draw: {fps} fps)";
-        }
-        private void ProcessUsingLockbitsAndUnsafeAndParallel(Bitmap processedBitmap, Color[,] colorArray)
-        {
-            unsafe
-            {
-                BitmapData bitmapData = processedBitmap.LockBits(new Rectangle(0, 0, processedBitmap.Width, processedBitmap.Height), ImageLockMode.ReadWrite, processedBitmap.PixelFormat);
-
-                int bytesPerPixel = System.Drawing.Bitmap.GetPixelFormatSize(processedBitmap.PixelFormat) / 8;
-                int heightInPixels = bitmapData.Height;
-                int widthInBytes = bitmapData.Width * bytesPerPixel;
-                byte* PtrFirstPixel = (byte*)bitmapData.Scan0;
-
-                Parallel.For(0, heightInPixels, y =>
-                {
-                    byte* currentLine = PtrFirstPixel + (y * bitmapData.Stride);
-                    for (int x = 0; x < widthInBytes; x = x + bytesPerPixel)
-                    {
-                        int oldBlue = currentLine[x];
-                        int oldGreen = currentLine[x + 1];
-                        int oldRed = currentLine[x + 2];
-
-                        int colorX = x / bytesPerPixel;
-                        currentLine[x] = colorArray[y, colorX].R;
-                        currentLine[x + 1] = colorArray[y, colorX].G;
-                        currentLine[x + 2] = colorArray[y, colorX].B;
-                    }
-                });
-
-                processedBitmap.UnlockBits(bitmapData);
-            }
         }
     }
 }
