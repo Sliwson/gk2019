25ad9765e2dd190c0d4de80c6e4db1702990bd90
Mateusz Sliwakowski <mateusz@thingtrunk.com>
2019-11-06 22:28:32 +0100

    Add new normalmap

--- gk2019/Lightning/Properties/Resources.Designer.cs
+++ gk2019/Lightning/Properties/Resources.Designer.cs
@@ -89,5 +89,15 @@ namespace Lightning.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap normal_3 {
+            get {
+                object obj = ResourceManager.GetObject("normal_3", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
