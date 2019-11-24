78653b2ba25c83818f20e7acb21afc212cc640eb
Mateusz Śliwakowski <mateusz_sliw@interia.pl>
2019-11-24 15:59:33 +0100

    Prototype variables class

---
+++ gk2019/Colors/Variables.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colors
+{
+    public enum ColorRepresentation
+    {
+        HSV,
+        YCbCr,
+        Lab
+    }
+
+    public struct Chromacity
+    {
+        public Chromacity(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public float X { get; set; }
+        public float Y { get; set; }
+    }
+
+    public enum ColorProfile
+    {
+        sRGB
+    }
+
+    public enum Illuminant
+    {
+        D65
+    }
+
+    public class LabSettings
+    {
+        public ColorProfile ColorProfile { get; set; }
+        public Illuminant Illuminant { get; set; }
+        public Chromacity RedPrimary { get; set; }
+        public Chromacity GreenPrimary { get; set; }
+        public Chromacity BluePrimary { get; set; }
+        public Chromacity WhitePoint { get; set; }
+        public float Gamma { get; set; }
+    }
+
+    class Variables
+    {
+        public ColorRepresentation ColorRepresentation { get; set; }
+        public LabSettings LabSettings { get; set; }
+    }
+}
