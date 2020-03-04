0f234e6a3c65a1dfdead2379aaf92e424a3a689c
Mateusz Śliwakowski <mateusz_sliw@interia.pl>
2020-03-04 10:26:49 +0100

    Work on point light in shader

--- gk2019/OpenGL/lights.h
+++ gk2019/OpenGL/lights.h
@@ -7,3 +7,8 @@ PointLight* GetSamplePointLight(Mesh* mesh)
 {
 	return new PointLight(mesh, { -0.6f, .7f, 1.8f }, { 1.f, 1.f, 0.5f }, glm::vec3(0.2f), glm::vec3(0.5f), glm::vec3(1.f));
 }
+
+SpotLight* GetSampleSpotLight()
+{
+	return new SpotLight(nullptr, { -0.6f, .7f, 1.8f }, { 1.f, 1.f, 0.5f }, glm::vec3(0.2f), glm::vec3(0.5f), glm::vec3(1.f), { 0, 1.f, 1.f }, glm::radians(12.5f));
+}
