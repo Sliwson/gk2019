319b5750553f369860c7444ad4afdaa3598b7b2c
Mateusz Śliwakowski <mateusz_sliw@interia.pl>
2020-03-05 19:14:56 +0100

    Work on scene placement

--- gk2019/OpenGL/bowlingModel.cpp
+++ gk2019/OpenGL/bowlingModel.cpp
@@ -11,7 +11,9 @@ void BowlingModel::Render(Shader* shader, Camera* camera, glm::mat4 model)
 	auto sphere = meshes[1].get();
 	auto pin = meshes[3].get();
 
-	pin->Draw(shader, camera, glm::mat4(1.f));
+	for (int y = 0; y < 3; y++)
+		for (int x = 0; x < y + 1; x++)
+			pin->Draw(shader, camera, glm::translate(glm::mat4(1.f), { x, 0.f, y }));
 
 	const auto radius = 3.f;
 	const auto t = time / 2.f;
